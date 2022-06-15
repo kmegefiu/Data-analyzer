17b39daf171b4ad9996f7de1d8879d0431b665b3
Kendy Megene <andymegene@gmail.com>
2022-06-14 20:04:43 -0400

    Update DataAnalyzer.java

--- DataAnalyzer.java
+++ DataAnalyzer.java
@@ -24,11 +24,6 @@ public class DataAnalyzer {
          System.out.println("Data Analyzer Growth Rate");
          System.out.println("-------------------------------------------------");
          
-         // your output format must match mine
-         // look at the video for see the out format
-         // the exponent value is e^x = total method class   
-         // you need to find the x... do not over think it, use the Math class 
-         // YOU PUT YOUR CODE HERE
          for(ExecutionInfo p: data){
             
            System.out.printf("%-3s %-3d %-5s %-10d %-5s %-10d %-8s %-3.3f\n","Index: ",p.getFibIndex(), "Fib value:",p.getFibValue(),
